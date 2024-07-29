58070086883447ec53a932a86688683822a3b2fd
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-07-29 19:16:15 +1000

    refactor(todo list): Import the Tasks class in the index

--- src/scripts/tasks.js
+++ src/scripts/tasks.js
@@ -1,15 +1,4 @@
-import { 
-    add,
-    endOfMonth, 
-    endOfToday, 
-    endOfWeek, 
-    endOfYesterday, 
-    startOfMonth, 
-    startOfToday, 
-    startOfTomorrow,
-    startOfWeek } from 'date-fns';
-
-class Task {
+export class Task {
     constructor(obj, id= 0) {
             this.id = id;
             this.title = obj.title != null ? obj.title : null;
@@ -26,7 +15,7 @@ class Task {
     }
 }
 
-class DateOnly {
+export class DateOnly {
     constructor(date = null) {
         if (date == null) {
             date = new Date();
@@ -65,86 +54,5 @@ class Project {
 
 }
 
-export class Tasks {
-    constructor(data) {
-        this.tasks = [];
 
-        for (let id in data) {
-            let currentTask = data[id];
-            this.tasks.push(new Task(currentTask, id));
-        }
-    }
-
-    get overdue() {
-        let startDate = new DateOnly(0);
-        let endDate = endOfYesterday();
-        let overdueTasks = this.#filterByDate(startDate, endDate);
-
-        return this.#sortByPriority(overdueTasks);
-    }
-    
-    get today() {
-        let startDate = startOfToday();
-        let endDate = endOfToday();
-        let todayTasks = this.#filterByDate(startDate, endDate);
-
-        return this.#sortByPriority(todayTasks);
-    }
-
-    get thisWeek() {
-        let startDate = startOfTomorrow();
-        let endDate = endOfWeek(startOfToday(), { weekStartsOn: 1 }); 
-        let thisWeekTasks = this.#filterByDate(startDate, endDate);
-
-        return this.#sortByPriority(thisWeekTasks);
-    }
-
-    get thisMonth() {
-        let thisWeek = startOfWeek(startOfToday(), { weekStartsOn: 1});
-        let nextWeek = add(thisWeek, {weeks: 1});
-
-        let startDate = nextWeek; 
-        let endDate = endOfMonth(startOfToday());
-        let thisMonthTasks = this.#filterByDate(startDate, endDate);
-
-        return this.#sortByPriority(thisMonthTasks);
-    }
-
-    get later() {
-        let thisMonth = startOfMonth(startOfToday());
-        let nextMonth = add(thisMonth, {months: 1});
-        let endOfTime = new DateOnly(8640000000000000);
-
-        let startDate = nextMonth;
-        let endDate = endOfTime;
-        let laterTasks = this.#filterByDate(startDate, endDate);
-
-        return this.#sortByPriority(laterTasks);
-    }
-
-    #sortByPriority(tasks = this.tasks) {
-        let tasksSorted = tasks.toSorted((a, b) => a.priority.index - b.priority.index);
-
-        return tasksSorted;
-    }
-
-    #filterByDate(startDate, endDate) {
-        startDate = new DateOnly(startDate);
-        endDate = new DateOnly(endDate);
-
-        let tasksFiltered = [];
-
-        for (let task in this.tasks) {
-            let currentTask = this.tasks[task];
-            let dueDate = new DateOnly(currentTask.dueDate);
-
-            if (dueDate >= startDate &&
-                dueDate <= endDate) {
-                    tasksFiltered.push(currentTask);
-                }
-        }
-
-        return tasksFiltered;
-    }
-}
 
