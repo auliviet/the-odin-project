c65f32369c3e6be0382ab3ec23c9312bae5fb8a2
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-08-01 13:53:08 +1000

    chore(todo list): remove unused logging

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -5,7 +5,6 @@ export class Storage {
     #testData = new Todo(data).tasks;
 
     constructor() {
-        console.log(this.#testData)
         if (this.#storageAvailable("localStorage")) {
             return this.#getStoredData();
         } else {
