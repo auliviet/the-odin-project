44508e12e72e71d9d91ad05653b7ec5dd89caa6c
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-08-21 15:43:50 +1000

    Prettify the file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Stylesheets
-import "./style.css"
+import "./style.css";
 
 // Pages
 import home from "./pages/home";
@@ -10,25 +10,24 @@ import about from "./pages/about";
 let buttons = document.querySelectorAll("button");
 
 for (let button of buttons) {
-    let id = button.id;
+  let id = button.id;
 
-    switch (id) {
-        case "home":
-            button.addEventListener("click", home);
-            break;
-        
-        case "menu":
-            button.addEventListener("click", menu);
-            break;
-        
-        case "about":
-            button.addEventListener("click", about);
-            break;
-    
-        default:
-            break;
-    }
+  switch (id) {
+    case "home":
+      button.addEventListener("click", home);
+      break;
+
+    case "menu":
+      button.addEventListener("click", menu);
+      break;
+
+    case "about":
+      button.addEventListener("click", about);
+      break;
+
+    default:
+      break;
+  }
 }
-
 
 home();
