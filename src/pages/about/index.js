ffa59f28b3f63068abdb42b4198133bdefa4b538
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-08-24 09:16:08 +1000

    Update About styles

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,4 +1,4 @@
-//import "./index.css";
+import "./index.css";
 
 export default function about() {
   let wrapper = document.querySelector("#content-wrapper");
@@ -8,20 +8,21 @@ export default function about() {
   container.textContent = "";
 
   let headings = document.createElement("div");
-  headings.className = "about__headings";
+  headings.className = "about__content";
 
   const title = document.createElement("h1");
-  title.textContent = "The best pizza in town.";
+  title.className = "about__title";
+  title.innerHTML = "The best pizza in town.<br>Since 1991.";
   headings.append(title);
 
-  const subTitle = document.createElement("h2");
-  subTitle.textContent = "Since 1991.";
-  headings.append(subTitle);
-
   const paragraph = document.createElement("p");
+  paragraph.className = "about__description";
   paragraph.innerHTML =
     "My journey began in my grandmother’s kitchen in Naples. Every Sunday, I would join her, kneading the dough and learning the secrets of her beloved recipes. Those afternoons were filled with love and the joy of family gathered around the table. <br>Pizzas have always represented special moments for me. A way to connect with loved ones. I strive to recreate that warmth and joy for everyone who walks through our doors. ";
   headings.append(paragraph);
 
-  container.append(headings);
+  let imageWrapper = document.createElement("div");
+  imageWrapper.className = "about__image-wrapper";
+
+  container.append(headings, imageWrapper);
 }
