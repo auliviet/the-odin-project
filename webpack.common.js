f5db6faaea114e968cc9ee8becc92402e42fe33b
auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-09 23:14:53 +1000

    chore(weather app): set-up project repository
    - Initial commit
    - Create the folder structure
    - Setup webpack
    - Setup node
    - Setup gitignore
    - Setup eslint

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/_assets", to: "assets" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /assets\/.*$/i,
+        type: "asset/resource",
+        /* do not emit copies of files */
+        generator: {
+          emit: false,
+        },
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+};
