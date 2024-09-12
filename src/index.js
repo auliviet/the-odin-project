9890cc908ba40d7bf0d938792efd1608a50a3e02
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-13 00:21:16 +1000

    feat(weather app): refresh the content of the page after a new search

--- src/index.js
+++ src/index.js
@@ -9,18 +9,28 @@ import Display from "./_components/_display";
 import { getWeather } from "./_services/async";
 
 // Main
-const weather = getWeather();
-weather.then(console.log, alert);
-
-const body = document.querySelector("body");
-body.innerHTML = `
-  ${new Display().render(weather)}
-`;
-
-// Events
-let button = document.querySelector("button");
-let input = document.querySelector("input");
-button.addEventListener("click", (event) => {
-  event.preventDefault();
-  getWeather(input.value).then(console.log);
-});
+const weather = getWeather()
+  .then((response) => {
+    console.log(response);
+    return response;
+  })
+  .then((response) => {
+    const body = document.querySelector("body");
+    body.innerHTML = `
+      ${Display.render(response)}
+    `;
+  })
+  .then(() => {
+    // Events
+    let button = document.querySelector("button");
+    let input = document.querySelector("#location");
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      getWeather(input.value).then((response) => {
+        Display.refresh(response);
+      });
+    });
+  })
+  .catch((error) => {
+    alert(error);
+  });
