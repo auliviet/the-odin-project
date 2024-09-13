6fccf32990a250226ba76a1b97a45d80a5f14dc9
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-14 09:54:12 +1000

    style(weather app): add style to WeatherForecast

--- src/_components/weatherForecast.js
+++ src/_components/weatherForecast.js
@@ -1,3 +1,5 @@
+import "./weatherForecast.css";
+
 export default class WeatherForecast {
   constructor({ forecast }) {
     this.forecast = forecast.slice(1, 8);
@@ -16,26 +18,29 @@ export default class WeatherForecast {
 
     return `
       <div class="weather-forecast__item">
-        <img 
-          class="weather-forecast__item-icon" 
-          src="_assets/icons/${data.icon}.svg" 
-          alt="${data.icon} icon"
-        />
-        <p class="weather-forecast__item-date">
-          ${date.getDate()}/${date.getMonth()}
-        </p>
-        <p class="weather-forecast__item-temp">
-          ${Math.round(data.tempmin)}º - ${Math.round(data.tempmax)}º
-        </p>
+        <div class="weather-forecast__item-icon">
+          <span class=${data.icon}></span>
+        </div>
+        <div class="weather-forecast__item-info">
+          <p class="weather-forecast__item-date">
+            ${date.getDate()}/${date.getMonth()}
+          </p>
+          <p class="weather-forecast__item-temp">
+            ${Math.round(data.tempmin)}º - ${Math.round(data.tempmax)}º
+          </p>
+        </div>
       </div>
     `;
   }
 
   render() {
     return `
-      ${this.#title()}
-      <div class="weather-forecast">
-        ${this.forecast.map((forecast) => this.#forecast(forecast)).join("")}
+      
+      <div class="weather-forecast large">
+        ${this.#title()}
+        <div class="weather-forecast__container">
+          ${this.forecast.map((forecast) => this.#forecast(forecast)).join("")}
+        </div>
       </div>
     `;
   }
