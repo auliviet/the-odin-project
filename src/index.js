2b9e1075486f547d95f2a55757e644e64656377f
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-13 21:02:07 +1000

    refactor(weather app): use async/wait on main function

--- src/index.js
+++ src/index.js
@@ -9,28 +9,30 @@ import Display from "./_components/_display";
 import { getWeather } from "./_services/async";
 
 // Main
-const weather = getWeather()
-  .then((response) => {
-    console.log(response);
-    return response;
-  })
-  .then((response) => {
-    const body = document.querySelector("body");
-    body.innerHTML = `
-      ${Display.render(response)}
-    `;
-  })
-  .then(() => {
-    // Events
-    let button = document.querySelector("button");
-    let input = document.querySelector("#location");
-    button.addEventListener("click", (event) => {
-      event.preventDefault();
-      getWeather(input.value).then((response) => {
-        Display.refresh(response);
-      });
-    });
-  })
-  .catch((error) => {
-    alert(error);
+const display = new Display();
+const body = document.querySelector("body");
+body.innerHTML = display.render();
+addEvents();
+
+async function Weather(location) {
+  display.loading();
+  try {
+    const weather = await getWeather(location);
+    display.refresh(weather);
+    addEvents();
+    console.log(weather);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Events
+function addEvents() {
+  let button = document.querySelector("button");
+  let input = document.querySelector("#location");
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(input.value);
+    Weather(input.value);
   });
+}
