e3cd2c863447db5b61f7789ed5c2f416829a8903
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-09-14 19:28:27 +1000

    style(weather app): add background color based on the weather

--- src/_components/_display.js
+++ src/_components/_display.js
@@ -6,6 +6,27 @@ import WeatherForecast from "./weatherForecast";
 export default class Display {
   refresh(data) {
     const body = document.querySelector("body");
+    switch (data.currentConditions.icon) {
+      case "clear-day":
+      case "partly-cloudy-day":
+        body.className = "yellow";
+        break;
+
+      case "rain":
+      case "wind":
+        body.className = "blue";
+        break;
+
+      case "clear-night":
+      case "partly-cloudy-night":
+        body.className = "black";
+        break;
+
+      default:
+        body.className = "grey";
+        break;
+    }
+
     body.innerHTML = `
       ${new Navigation(data).render()}
       ${new Search(data).render()}
