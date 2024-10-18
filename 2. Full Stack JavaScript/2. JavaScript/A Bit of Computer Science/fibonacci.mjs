fe022caef177e5cb7543cdee2c69ada84882c2fd
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-10-18 18:51:02 +0800

    Add tests for Fibonacci

--- 2. Full Stack JavaScript/2. JavaScript/A Bit of Computer Science/fibonacci.mjs
+++ 2. Full Stack JavaScript/2. JavaScript/A Bit of Computer Science/fibonacci.mjs
@@ -1,6 +1,6 @@
-function fibonacci(n) {
-  if (n == 0) return 0;
+export default function fibonacci(n) {
   if (n == 1) return 1;
+  if (n == 2) return 1;
 
   return fibonacci(n - 2) + fibonacci(n - 1);
 }
