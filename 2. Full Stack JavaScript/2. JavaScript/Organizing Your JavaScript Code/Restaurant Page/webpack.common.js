7b9da52a6ab9f2b403ae7b4a84f16c4006da7a61
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-11-01 08:13:57 +1030

    chore(restaurant page): : set-up project repository
    - Initial commit
    - Setup webpack
    - Setup npm
    - Setup eslit

---
+++ 2. Full Stack JavaScript/2. JavaScript/Organizing Your JavaScript Code/Restaurant Page/webpack.common.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/assets/img", to: "assets/img" }],
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
