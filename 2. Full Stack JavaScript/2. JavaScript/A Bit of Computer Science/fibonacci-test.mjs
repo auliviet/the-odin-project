d2704d656bc740c65e9a1a145562e1e165ee753c
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-11-01 20:00:08 +1030

    test(fibonacci): create test functions

---
+++ 2. Full Stack JavaScript/2. JavaScript/A Bit of Computer Science/fibonacci-test.mjs
@@ -0,0 +1,4 @@
+import fibonacci from "./fibonacci.mjs";
+
+console.log("The 8th number in Fibonacci's sequence is: ", fibonacci(8));
+console.log("The 10th number in Fibonacci's sequence is: ", fibonacci(10));
